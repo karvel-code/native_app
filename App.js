11f5333ecd94e869d39d7c02e24a5befd7f20c95
karvel-code <velgithinji@gmail.com>
2022-07-09 14:46:31 +0300

    Add image to app and try out the diffbehaviors

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
   let x = 1;
   return (
     <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image source={{
+            uri: 'https://picsum.photos/200/300',
+          }} style={{width: 200, height: 200}} blurRadius={10} fadeDuration={1000}>
+
+        </Image>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -15,7 +20,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  // image: {
+  //   width: "200px",
+  //   height: "200px"
+  // }
 });
